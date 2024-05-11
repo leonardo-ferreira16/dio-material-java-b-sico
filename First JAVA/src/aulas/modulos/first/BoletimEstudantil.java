8433c93fdb6c4edf6970f55fde70b6c373cfa545
Leonardo Ferreira <eng.leonardo.abreu@gmail.com>
2024-05-11 12:32:02 -0300

    Commitando novas classes do primeiro módulo

---
+++ First JAVA/src/aulas/modulos/first/BoletimEstudantil.java
@@ -0,0 +1,14 @@
+package aulas.modulos.first;
+
+public class BoletimEstudantil {
+    public static void main(String [] args){
+        int mediaFinal = 7;
+        if(mediaFinal<7){
+            System.out.println("REPROVADO!");
+        }else if(mediaFinal==7){
+            System.out.println("Passou raspando!");
+        } else {
+            System.out.println("APROVADO!");
+        }
+    }
+}
---
+++ First JAVA/src/aulas/modulos/first/TiposVariaveis.java
@@ -0,0 +1,41 @@
+package aulas.modulos.first;
+public class TiposVariaveis {
+
+    public static void main (String [] args){
+        /* 
+        System.out.print("Hello, Guys!");
+        //Tipos de variável:
+        String nome = "Leo";
+        int idade = 21;
+        boolean verdadeira = true;
+        double salario = 2500;
+        float altura = 1.70F;
+        short ano = 2024;
+        byte idade2 = 123;
+        long cpf = 12345678900L;
+        */
+
+        /* 
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        */
+
+        /*Declarando como uma constante que não pode ser alterada:
+
+        final int NUMERO = 5;
+        NUMERO = 10;
+        System.out.print(NUMERO);
+        */
+        
+        String primeiroNome = "Leonardo";
+        String segundoNome = "Ferreira";
+        String completedName = nomeCompleto (primeiroNome, segundoNome);
+        System.out.println(completedName);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
+        
+    }
+}
